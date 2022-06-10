1b8e092dd739e7998ddf5e415578d233c6c2ea72
simonecpc <simone101199@gmail.com>
2022-06-10 15:17:06 +0200

    esercizio svolto 1

--- src/jana60/FizzBuzz.java
+++ src/jana60/FizzBuzz.java
@@ -1,5 +1,29 @@
 package jana60;
 
+import java.util.function.IntBinaryOperator;
+
 public class FizzBuzz {
 
+	public static void main(String[] args) {
+		
+		for (int i = 1; i <= 100; i++) {
+			
+			if (i % 3 == 0 && i % 5 == 0) {
+				System.out.println("Fizz Buzz");
+			}
+			else if (i % 3 == 0) {
+				System.out.println("Fizz");
+			}
+			else if (i % 5 ==0) {
+				System.out.println("Buzz");
+			}
+			
+			else {
+				System.out.println(i);
+			}
+			int b;
+		}
+		
+	}
+	
 }
